7141b02d7c26b52f2d1eff57a33c48bc31d744f6
naufal gahara <rifqinaufal70@gmail.com>
2023-11-08 22:53:20 +0700

    donnut chart

---
+++ app/src/main/java/com/example/sdd_project/NewActivity.kt
@@ -0,0 +1,122 @@
+package com.example.sdd_project
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+class NewActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+
+                        var strUser: ArrayList<String>? = intent.getStringArrayListExtra("value_label")
+                        strUser?.let { DoughnutChart(it)
+                            Log.d("TAG", "onCreate: newact$it")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun DoughnutChart(
+    str: List<String>,
+    values: List<Float> = listOf(65f, 40f, 25f, 20f),
+    colors: List<Color> = listOf(
+        Color(0xFFFF6384),
+        Color(0xFFFFCE56),
+        Color(0xFF36A2EB),
+        Color(0xFF448AFF)
+    ),
+    legend: List<String> = str,
+    size: Dp = 200.dp,
+    thickness: Dp = 36.dp
+) {
+    val sumOfValues = values.sum()
+    val proportions = values.map {
+        it * 100 / sumOfValues
+    }
+    val sweepAngles = proportions.map {
+        360 * it / 100
+    }
+
+    Canvas(
+        modifier = Modifier.size(size = size)
+    ) {
+        var startAngle = -90f
+
+        for (i in values.indices) {
+            drawArc(
+                color = colors[i],
+                startAngle = startAngle,
+                sweepAngle = sweepAngles[i],
+                useCenter = false,
+                style = Stroke(width = thickness.value, cap = StrokeCap.Butt)
+            )
+            startAngle += sweepAngles[i]
+        }
+    }
+    Spacer(modifier = Modifier.height(32.dp))
+    Column {
+        for (i in values.indices) {
+            DisplayLegend(color = colors[i], legend = legend[i])
+        }
+    }
+}
+
+@Composable
+fun DisplayLegend(color: Color, legend: String) {
+    Row(
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Box(
+            modifier = Modifier
+                .size(10.dp)
+                .background(color = color, shape = CircleShape)
+        )
+
+        Spacer(modifier = Modifier.width(4.dp))
+
+        Text(
+            text = legend,
+            color = Color.Black
+        )
+    }
+}
--- app/src/main/java/com/example/sdd_project/PortofolioScreen.kt
+++ app/src/main/java/com/example/sdd_project/PortofolioScreen.kt
@@ -1,6 +1,13 @@
 package com.example.sdd_project
 
+import android.content.Context
+import android.content.Intent
 import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -10,24 +17,37 @@ import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import com.example.sdd_project.data.DonutChartData
-import com.example.sdd_project.data.LineChartData
-import com.example.sdd_project.view_model.PortofolioViewModel
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.sdd_project.data.DonutChartData
+import com.example.sdd_project.view_model.PortofolioViewModel
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
 
 @Composable
 fun PortofolioScreen(portofolioViewModel: PortofolioViewModel) {
     val portofolioData by portofolioViewModel.portofolioData.collectAsState()
-    Log.d("TAG", "PortofolioScreen: " + portofolioData.size)
+    var list: List<String> = listOf()
+    var jsontoArray:List<Int> = listOf()
 
+    Log.d("TAG", "PortofolioScreen: " + portofolioData.size)
+    val startActivityLauncher: ActivityResultLauncher<Intent> =
+        rememberLauncherForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == ComponentActivity.RESULT_OK) {
+            }
+        }
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -37,45 +57,53 @@ fun PortofolioScreen(portofolioViewModel: PortofolioViewModel) {
             item.data.forEach { idata ->
                 when (idata.type) {
                     "donutChart" -> {
-                        Log.d("TAG", "PortofolioScreen: ${idata.type}")
-                        if (idata.data is DonutChartData) {
-                            val donutChartData = item.data as DonutChartData
-                            DonutChart(donutChartData)
+                        val dataDonut: List<DonutChartData> = idata.data as List<DonutChartData>
+                        Log.d("TAG", "PortofolioScreenaaa: $dataDonut")
+                        Log.d("TAG", "PortofolioScreenaaa id: ${idata.data}")
+
+                        val gson = Gson()
+                        val listType = object : TypeToken<List<DonutChartData>>() {}.type
+                        val jsonDonutString: String = gson.toJson(dataDonut)
+                        val donutChartDataList: List<DonutChartData> = gson.fromJson(jsonDonutString, listType)
+                        Log.d("TAG", "PortofolioScreen: donutChartDataList $donutChartDataList")
+
+                        for (donutChartDataLista in donutChartDataList) {
+                            println("Label: ${donutChartDataLista.label}")
+                            println("Percentage: ${donutChartDataLista.percentage}")
+                            println("Data:")
+                            list = list + donutChartDataLista.label
+                            for (entry in donutChartDataLista.data) {
+                                println("Date: ${entry.trx_date}, Nominal: ${entry.nominal}")
+                            }
                         }
                     }
                     "lineChart" -> {
-                        Log.d("TAG", "PortofolioScreen: ${idata.type}")
-                        if (idata.data is LineChartData) {
-                            val lineChartData = item.data as LineChartData
-                            LineChart(lineChartData)
-                        }
+                        Log.d("TAG", "PortofolioScreen: ${idata.data}")
+                        //parsing
+                        jsontoArray = jsonStringToArrayLine(idata.data.toString())
+                        Log.d("parsing", "PortofolioScreen: $jsontoArray")
                     }
                     else -> {
                         Log.d("TAG", "PortofolioScreen: Unknown type")
-                        DefaultUI()
                     }
                 }
-
             }
+            Log.d("TAG", "PortofolioScreen list: $list")
+            DefaultUI(list, jsontoArray, startActivityLauncher, LocalContext.current)
         }
     }
 }
 
-@Composable
-fun DonutChart(data: DonutChartData) {
-    // Implement your Donut Chart UI here
-    Log.d("TAG", "DonutChart: $data")
+fun jsonStringToArrayLine(jsonString: String): List<Int> {
+    val gson = Gson()
+    val type = object : TypeToken<Map<String, List<Int>>>() {}.type
+    val jsonObject = gson.fromJson<Map<String, List<Int>>>(jsonString, type)
+    return jsonObject["month"] ?: emptyList()
 }
 
 @Composable
-fun LineChart(data: LineChartData) {
-    // Implement your Donut Chart UI here
-    Log.d("TAG", "LineChart: $data")
-}
-@Composable
-fun DefaultUI() {
+fun DefaultUI(array: List<String>, dataList: List<Int>, startActivityLauncher: ActivityResultLauncher<Intent>, context: Context) {
     var text by remember { mutableStateOf("Hello, World!") }
-
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -98,17 +126,88 @@ fun DefaultUI() {
 
         Button(
             onClick = {
-                text = "Testing!"
+                val intent = Intent(context, NewActivity::class.java)
+                val valueLabel = array.toCollection(ArrayList())
+                Log.d("TAG", "DefaultUI valueLabel: $valueLabel")
+                intent.putStringArrayListExtra("value_label", valueLabel)
+                startActivityLauncher.launch(intent)
             },
             modifier = Modifier.padding(top = 16.dp)
         ) {
-            Text(text = "Click here")
+            Text(text = "Click here to donut chart")
+        }
+
+        //Grafik Linechart
+        Canvas(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(30.dp)
+        ) {
+            // Draw X-axis
+            drawLine(
+                start = Offset(0f, size.height),
+                end = Offset(size.width, size.height),
+                color = Color.Black,
+                strokeWidth = 2f
+            )
+
+            // Draw Y-axis
+            drawLine(
+                start = Offset(0f, 0f),
+                end = Offset(0f, size.height),
+                color = Color.Black,
+                strokeWidth = 2f
+            )
+
+            // Draw line chart with text labels
+            if (dataList.isNotEmpty()) {
+                val maxY = dataList.maxOrNull() ?: 1
+                val scaleX = size.width / (dataList.size - 1).toFloat()
+                val scaleY = size.height / maxY.toFloat()
+
+                dataList.forEachIndexed { index, value ->
+                    val x = index * scaleX
+                    val y = size.height - value * scaleY
+
+                    // Draw points
+                    drawCircle(
+                        color = Color.Black,
+                        center = Offset(x, y),
+                        radius = 4f
+                    )
+
+                    // Draw text labels at each point
+                    drawIntoCanvas {
+                        it.nativeCanvas.drawText(
+                            value.toString(),
+                            x - 10f, // Adjust the x-coordinate for centering
+                            y - 10f, // Adjust the y-coordinate for centering
+                            android.graphics.Paint().apply {
+                                color = android.graphics.Color.BLACK
+                                textSize = 20f // Adjust the text size as needed
+                            }
+                        )
+                    }
+
+                    // Draw connecting lines
+                    if (index < dataList.size - 1) {
+                        val nextX = (index + 1) * scaleX
+                        val nextY = size.height - dataList[index + 1] * scaleY
+
+                        drawLine(
+                            start = Offset(x, y),
+                            end = Offset(nextX, nextY),
+                            color = Color.Black,
+                            strokeWidth = 2f
+                        )
+                    }
+                }
+            }
         }
     }
 }
-
 @Preview
 @Composable
 fun DefaultUIPreview() {
-    DefaultUI()
+//    DefaultUI()
 }
